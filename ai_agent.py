7301781a2f7a1b9a3ba37d6fe1e5b1e77c1e335f
Pratham Agarwal <prathamagarwal815@gmail.com>
2025-10-25 18:10:50 +0530

    Refactor AI agent response handling into a dedicated function for improved modularity

--- ai_agent.py
+++ ai_agent.py
@@ -11,22 +11,27 @@ from langchain_tavily import TavilySearch
 openai_llm=ChatOpenAI(model="gpt-4o-mini")
 groq_llm=ChatGroq(model="llama-3.3-70b-versatile")
 
-search_tool=TavilySearch(max_results=2)
 
 from langchain.agents import create_agent
 from langchain_core.messages.ai import AIMessage
 
 system_prompt="Act as an AI chatbot who is smart and friendly"
 
-agent = create_agent(
-    model=groq_llm,
-    tools=[search_tool],
-    system_prompt=system_prompt,
-)
+def get_response_from_ai_agent(llm_id, query, allow_search, system_prompt, provider):
+    if provider=="Groq":
+        llm=ChatGroq(model=llm_id)
+    elif provider=="OpenAI":
+        llm=ChatOpenAI(model=llm_id)
 
-query = "Tell me about the trends in AI"
-state = {"messages": query}
-response = agent.invoke(state)
-messages = response.get("messages")
-ai_messages=[message.content for message in messages if isinstance(message, AIMessage)]
-print(ai_messages[-1])
+    tools=[TavilySearch(max_results=2)] if allow_search else []
+
+    agent = create_agent(
+        model=llm,
+        tools=tools,
+        system_prompt=system_prompt,
+    )
+    state = {"messages": query}
+    response = agent.invoke(state)
+    messages = response.get("messages")
+    ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+    return ai_messages[-1]  
